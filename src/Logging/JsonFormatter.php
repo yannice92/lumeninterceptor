1dac0ef8d7c603f748979d702c18f96faba79f6f
Fernando Yannice <yannice92@gmail.com>
2020-04-19 18:49:46 +0700

    Add JsonFormatter.php to format datetime object

---
+++ src/Logging/JsonFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Yannice92\LumenInterceptor\Logging;
+
+
+use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
+
+class JsonFormatter extends \Monolog\Formatter\JsonFormatter
+{
+    public function __construct(int $batchMode = \Monolog\Formatter\JsonFormatter::BATCH_MODE_JSON, bool $appendNewline = true)
+    {
+        parent::__construct($batchMode, $appendNewline);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @suppress PhanTypeComparisonToArray
+     */
+    public function format(array $record): string
+    {
+        $normalized = $this->normalize($record);
+        $normalized['datetime'] = $normalized['datetime']->setTimezone(new \DateTimeZone('UTC'))->format(\DateTime::ISO8601);
+        if (isset($normalized['context']) && $normalized['context'] === []) {
+            $normalized['context'] = new \stdClass;
+        }
+        if (isset($normalized['extra']) && $normalized['extra'] === []) {
+            $normalized['extra'] = new \stdClass;
+        }
+
+        return $this->toJson($normalized, true) . ($this->appendNewline ? "\n" : '');
+    }
+}
